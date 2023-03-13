e7095273bcedd9d6da257caa531c5bddc89e0801
Akhirahstrong <codeclubdetroit@gmail.com>
2023-03-13 06:09:39 -0400

    first commit

---
+++ function.js
@@ -0,0 +1,16 @@
+console.log("It works");
+//console.warn("This is only a test!");
+
+var myName = "Akhirah";
+let workFromHome = true;
+let side = 15;
+let radius = 10;
+
+console.log("My name is " + myName, "I work from home: " + workFromHome, ", The length is: " + side, ", The radius is: " + radius);
+
+let travelOptions = [ "foot", "bike", "car", "airplane" ];
+console.log("The travel options are:");
+console.log(`1) ${travelOptions[0]}`);
+console.log(`2) ${travelOptions[1]}`);
+console.log(`3) ${travelOptions[2]}`);
+console.log(`4) ${travelOptions[3]}`);
